24a77d955c2ca369ee9f4f5d094c17033d81c33c
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-28 18:46:15 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uber-eats-clone-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
