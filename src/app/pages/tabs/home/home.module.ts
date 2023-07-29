b47c3f57c72fa7dae7d546e7c477e2007c178cfc
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-29 11:18:29 +0200

    Add banner component

--- src/app/pages/tabs/home/home.module.ts
+++ src/app/pages/tabs/home/home.module.ts
@@ -1,16 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+
+import { ComponentsModule } from 'src/app/components/components.module';
+import { BannerComponent } from 'src/app/components/banner/banner.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
-  declarations: [HomePage],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    ComponentsModule,
+  ],
+  declarations: [HomePage, BannerComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
