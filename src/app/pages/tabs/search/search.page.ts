d0f63bc5edf7d094dd4e428a143c83361ba7abc9
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-05 14:05:06 +0200

    Search page design

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  allRestaurants: any[] = [
+    {
+      cover: 'assets/imgs/1.jpg',
+      name: 'Stayfit',
+      short_name: 'stayfit',
+      cooking: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      distance: 2.5,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/2.jpg',
+      name: 'Stayfit1',
+      short_name: 'stayfit1',
+      cooking: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      distance: 2.5,
+      price: 100,
+    },
+    {
+      cover: 'assets/imgs/3.jpg',
+      name: 'Stayfit2',
+      short_name: 'stayfit2',
+      cooking: ['Italian', 'Mexican'],
+      rating: 5,
+      delivery_time: 25,
+      distance: 2.5,
+      price: 100,
+    },
+  ];
+  @ViewChild('searchInput') searchInput: any;
+  query: string = '';
+  restaurants: any[] = [];
+  isLoading: boolean = false;
+  model: any = {
+    icon: 'search-outline',
+    title: 'No restaurants record found',
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    setTimeout(() => {
+      this.searchInput.setFocus();
+    }, 500);
   }
 
+  async onSearchChange(event: any) {
+    this.query = event.detail.value;
+
+    if (this.query) {
+      this.isLoading = true;
+      setTimeout(async () => {
+        this.restaurants = this.allRestaurants.filter((restaurant) =>
+          restaurant.short_name.includes(this.query)
+        );
+        this.isLoading = false;
+      }, 3000);
+    }
+  }
 }
